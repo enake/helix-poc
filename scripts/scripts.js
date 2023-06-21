4c87217739cea850f7378c0d7f38a9c7ecf06b56
vladalexandruradulescu <vlad---alexandru.radulescu@teliacompany.com>
2023-06-21 16:23:34 +0300

    c-teaser-card - partial

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -402,9 +402,10 @@ const addScript = src => {
 }
 
 function initBaseUri() {
+  const domainName = 'bitdefender';
   const domainExtension = window.location.hostname.split('.').pop(); // com | ro | other
 
-  window.BASE_URI = ['com', 'ro'].includes(domainExtension) ? domainExtension : 'com';
+  window.BASE_URI = ['com', 'ro'].includes(domainExtension) ? `${window.location.protocol}//www.${domainName}.${domainExtension}` : `${window.location.protocol}//www.${domainName}.com`;
 }
 
 initMobileDetector('mobile');
