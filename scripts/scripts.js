83235fa26c80fe32df67af03d2a0d790a1af1a16
vladalexandruradulescu <vlad---alexandru.radulescu@teliacompany.com>
2023-06-24 00:36:07 +0300

    update columns block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -375,19 +375,6 @@ export const isView = viewport => {
   return !!(element && getComputedStyle(element).display !== 'none');
 }
 
-
-// Create the link element
-var linkElement = document.createElement('link');
-
-// Set the attributes of the link element
-linkElement.rel = 'stylesheet';
-linkElement.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css';
-linkElement.integrity = 'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65';
-linkElement.crossOrigin = 'anonymous';
-
-// Inject the link element into the head tag
-document.head.appendChild(linkElement);
-
 // add new script file
 const addScript = src => {
   return new Promise((resolve, reject) => {
