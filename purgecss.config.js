458b32bc30a827f92984b6d4ac9b51674f93ba4d
vladalexandruradulescu <vlad---alexandru.radulescu@teliacompany.com>
2023-07-04 02:53:23 +0300

    carousel animation fix

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,4 +2,20 @@ module.exports = {
   content: ['404.html', 'blocks/**/*.js', 'scripts/*.js'], // Files to analyze for used CSS classes
   css: ['node_modules/bootstrap/dist/css/bootstrap.min.css'], // CSS files to include
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  safelist: [
+    'carousel',
+    'carousel-inner',
+    'carousel-dark',
+    'carousel-fade',
+    'carousel-item-next',
+    'carousel-item-start',
+    'carousel-item-end',
+    'carousel-item-prev',
+    'carousel-control-prev',
+    'carousel-control-next',
+    'carousel-control-start',
+    'carousel-control-end',
+    'carousel-control-next-icon',
+    'carousel-control-prev-icon',
+  ],
 };
