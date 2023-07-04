415a95aeaaa726926c701097bc844915b1e4ca08
ltiseanu <ltiseanu@bitdefender.com>
2023-07-04 12:18:11 +0300

    Updates on c-dropdownbox

--- blocks/c-dropdownbox/c-dropdownbox.js
+++ blocks/c-dropdownbox/c-dropdownbox.js
@@ -9,6 +9,10 @@
 */
 
 export default function decorate(block) {
+  const parentSelector = block.closest('.section');
+  const metaData = parentSelector.dataset;
+  const { bottomText } = metaData;
+
   // search for [] to replace with span greeenTag class
   const getFirstDivs = block.querySelectorAll('.c-dropdownbox-container .block > div > div:nth-child(1)');
   [].forEach.call(getFirstDivs, (item) => {
@@ -23,6 +27,14 @@ export default function decorate(block) {
       tab.parentNode.classList.toggle('inactive');
     });
   });
+
+  // add bottom text if is set
+  if (typeof metaData.bottomText !== 'undefined') {
+    const bottomLine = document.createElement('p');
+    bottomLine.innerHTML = metaData.bottomText;
+    bottomLine.classList = 'bottomText'
+    block.append(bottomLine)
+  }
 
   // if 3rd div does not exists => has 2 elements
   if (!block.querySelector('.c-dropdownbox-container .block > div > div:nth-child(3)')) {
