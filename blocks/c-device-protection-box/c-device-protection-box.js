e22ae38060ac0985895ce6a20e3c388359b9cd39
ltiseanu <ltiseanu@bitdefender.com>
2023-07-07 11:52:13 +0300

    Fixes for approval

--- blocks/c-device-protection-box/c-device-protection-box.js
+++ blocks/c-device-protection-box/c-device-protection-box.js
@@ -25,12 +25,12 @@ export default function decorate(block) {
   const rowText = [...block.children].slice(1, lastElIndex);
 
   block.innerHTML = `
-    <div class="container">
+    <div class="container-fluid">
       <div class="row">
         <div class="col-12 col-lg-6">
           ${pictureEl.innerHTML}
         </div>
-        <div class="col-12 col-lg-6 mt-3">
+        <div class="col-12 col-lg-4 mt-3">
           ${rowText.map((row) => `
             <div class="d-flex row-box">
               <div>${new SvgLoaderComponent(row.children[0].innerText, svgColor, svgSize).render()}</div>
