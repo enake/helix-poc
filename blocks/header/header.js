8bbd4d06d8a723ebe4c11b8a9faa9be227743b32
vladalexandruradulescu <vlad---alexandru.radulescu@teliacompany.com>
2023-07-10 20:36:34 +0300

    logo link

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,5 @@
 import { getMetadata, decorateIcons2 } from '../../scripts/lib-franklin.js';
+import { instance } from "../../scripts/utils.js";
 
 async function extractSpanSvg(html) {
   const div = document.createElement('div');
@@ -22,11 +23,13 @@ export default async function decorate(block) {
     const html = await resp.text();
 
     const spanSvg = await extractSpanSvg(html);
+    const dynamicLanguage = instance === 'dev' ? 'com' : window.location.hostname.split('.').pop();
+    const homeUrl = `https://www.bitdefender.${dynamicLanguage}/`;
 
     block.className = 'lp-header py-3';
     block.innerHTML = `
       <div class="container">
-        <a class="d-inline-block" href="/">
+        <a class="d-inline-block" href="/${homeUrl}">
           ${spanSvg.outerHTML}
         </a>
       </div>`;
